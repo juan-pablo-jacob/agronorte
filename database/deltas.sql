62a155380b1c76bb3c857b267b329a23f2aff5ef
jpjacob <jjacob@santafe.gov.ar>
2018-01-29 11:43:34 -0300

    Commit con deltas de la BD. Correr esto en producción

--- database/deltas.sql
+++ database/deltas.sql
@@ -1,2 +1,14 @@
 ALTER TABLE `cotizacion`
 ADD COLUMN `precio_lista_producto_iva`  double NULL DEFAULT NULL AFTER `precio_toma_iva`;
+
+
+--Actualización de los precio de venta
+update cotizacion
+set precio_venta = (100 - descuento) * precio_lista_producto / 100
+where is_toma <> 1;
+
+--actualización de precio venta IVA y precio lista producto IVA
+update cotizacion
+set precio_venta_iva = precio_venta * (100 + 10.5) / 100,
+precio_lista_producto_iva = precio_lista_producto * (100 + 10.5) / 100
+where is_toma <> 1;
